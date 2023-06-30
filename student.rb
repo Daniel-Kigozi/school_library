440f4009c6187075ab338bdedabfea320ec0d3ac
Daniel-Kigozi <kdanalex69@gmail.com>
2023-06-30 10:44:11 +0300

    Fix linter errors3

--- student.rb
+++ student.rb
@@ -12,7 +12,7 @@ class Student < Person
     '¯\\_(ツ)_/¯'
   end
 
-  def classroom=(classroom)def classroom=(classroom)
+  def classroom=(classroom)
     @classroom = classroom
     classroom.students.push(self) unless classroom.students.include?(self)
   end
